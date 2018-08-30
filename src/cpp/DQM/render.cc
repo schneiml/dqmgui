5207d59e00669bfbc25b8a5f552b570dab71cfaf
Marcel Andre Schneider <marcel.andre.schneider@cern.ch>
2018-08-30 17:39:37 +0200

    Convert pull-val code from autoDQM to C++

--- src/cpp/DQM/render.cc
+++ src/cpp/DQM/render.cc
@@ -39,6 +39,8 @@
 #include "TPaveStats.h"
 #include "TProfile2D.h"
 #include "THStack.h"
+#include "Math/Math.h"
+#include "Math/QuantFuncMathCore.h"
 #include <fstream>
 #include <sstream>
 #include <iostream>
@@ -1671,6 +1673,141 @@ private:
       }
     }
     c.cd();
+  }
+
+  // ----------------------------------------------------------------------
+  // Render a pull-value comaprison of object and reference
+  void
+  doRenderPullValComparison(TCanvas &c,
+                          VisDQMImgInfo &i,
+                          VisDQMObject *objs,
+                          size_t numobjs,
+                          const VisDQMRenderInfo &ri,
+                          std::vector<TObject *> &nukem)
+  {
+    //Do normal rendering except if there is exactly one reference
+    auto data_hist = dynamic_cast<TH2*>(objs[0].object);
+    auto ref_hist = dynamic_cast<TH2*>(objs[0].reference);
+    if (1 //num_objects != 1 
+      || !data_hist || data_hist->GetDimension() != 2
+      || !ref_hist || ref_hist->GetDimension() != 2
+      || ref_hist->GetNbinsX() != data_hist->GetNbinsX()
+      || ref_hist->GetNbinsY() != data_hist->GetNbinsY() )
+    {
+      doRenderStacked(c, i, objs, numobjs, ri, nukem);
+      return;
+    }
+
+    // copy all objects, such that we can safely modify pulls[0].
+    std::vector<VisDQMObject> pulls(objs, objs+numobjs);
+
+    auto ref_profile = dynamic_cast<TProfile2D*>(ref_hist);
+    auto data_profile = dynamic_cast<TProfile2D*>(data_hist);
+    // Get empty clone of reference histogram for pull hist
+    TH2* pull_hist;
+    if (ref_profile != nullptr) {
+      pull_hist = ref_profile->ProjectionXY("pull_hist");
+    } else {
+      pull_hist = dynamic_cast<TH2*> (ref_hist->Clone("pull_hist"));
+    }
+    nukem.push_back(pull_hist);
+    
+    pull_hist->Reset();
+    
+    // Reject empty histograms
+    //auto min_entries = 1000;
+    //auto is_good = data_hist->GetEntries() != 0 && data_hist->GetEntries() >= min_entries;
+
+    // Normalize data_hist
+    if (data_hist->GetEntries() > 0) {
+      data_hist->Scale(ref_hist->GetSumOfWeights() / data_hist->GetSumOfWeights());
+    }
+
+    double pull_cap = 25;
+    double max_pull = 0.0;
+    for (auto x = 1; x <= ref_hist->GetNbinsX(); x++) {
+      for (auto y = 1; y <= ref_hist->GetNbinsY(); y++){
+        auto bin1 = data_hist->GetBinContent(x, y);
+        auto bin2 = ref_hist->GetBinContent(x, y);
+
+        // Proper Poisson error
+        double bin1err, bin2err;
+        if (data_profile && ref_profile) {
+          bin1err = data_profile->GetBinError(x, y);
+          bin2err = ref_profile->GetBinError(x, y);
+        } else {
+          std::tie(bin1err, bin2err) = get_poisson_errors(bin1, bin2);
+        }
+
+        double new_pull;
+        if (bin1err == 0 && bin2err == 0) {
+          new_pull = 0;
+        } else {
+          new_pull = pull(bin1, bin1err, bin2, bin2err);
+        }
+
+        // Check if max_pull
+        max_pull = std::max(max_pull, std::abs(new_pull));
+
+        // Clamp the displayed value
+        auto fill_val = std::max(std::min(new_pull, pull_cap), -pull_cap);
+
+        // If the input bins were explicitly empty, make this bin white by
+        // setting it out of range
+        if (bin1 == 0 && bin2 == 0) {
+          fill_val = -999;
+        }
+
+        pull_hist->SetBinContent(x, y, fill_val);
+      }
+    }
+
+    pull_hist->GetZaxis()->SetRangeUser(-(pull_cap), pull_cap);
+    pull_hist->SetTitle(pull_hist->GetTitle());
+    gStyle->SetPalette(kLightTemperature);
+    gStyle->SetNumberContours(255);
+    pull_hist->SetOption("colz");
+
+    pulls[0].object = pull_hist;
+    pulls[0].reference = nullptr;
+    doRenderOrdinary(c, i, &pulls[0], numobjs, ri, nukem);
+  }
+
+  double
+  pull(double bin1, double binerr1, double bin2, double binerr2) {
+    return (bin1 - bin2) / (sqrt(binerr1*binerr1 + binerr2*binerr2));
+  }
+
+  std::tuple<double, double>
+  get_poisson_errors(double bin1, double bin2) {
+    auto alpha = 1 - 0.6827;
+    double m_error1, m_error2, p_error1, p_error2;
+    if (bin1 == 0) {
+      m_error1 = 0;
+      p_error1 = ROOT::Math::gamma_quantile_c(alpha / 2, bin1 + 1, 1);
+    } else {
+      m_error1 = ROOT::Math::gamma_quantile(alpha / 2, bin1, 1);
+      p_error1 = ROOT::Math::gamma_quantile_c(alpha / 2, bin1 + 1, 1);
+    }
+
+    if (bin2 == 0) {
+      m_error2 = 0;
+      p_error2 = ROOT::Math::gamma_quantile_c(alpha / 2, bin2 + 1, 1);
+    } else {
+      m_error2 = ROOT::Math::gamma_quantile(alpha / 2, bin2, 1);
+      p_error2 = ROOT::Math::gamma_quantile_c(alpha / 2, bin2 + 1, 1);
+    }
+
+    double bin1err, bin2err;
+    if (bin1 > bin2) {
+      bin1err = bin1 - m_error1;
+      bin2err = p_error2 - bin2;
+    } else {
+      bin2err = bin2 - m_error2;
+      bin1err = p_error1 - bin1;
+    }
+
+    return std::make_tuple(bin1err, bin2err);
   }
 
   // ----------------------------------------------------------------------
